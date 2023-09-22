5633a2f25fa43a55949198fa6bbfcc480d1e0a8a
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-09-22 16:51:14 +0200

    V3

--- eclipse-workspace/EX05 Java Detectar SO/src/ClaseMain.java
+++ eclipse-workspace/EX05 Java Detectar SO/src/ClaseMain.java
@@ -1,13 +1,16 @@
 /**
  * 
  */
-
+import java.util.*;
 /**
  * 
  */
 final class ClaseMain {
 	
 	private static String nombreSO = System.getProperty("os.name").toLowerCase();
+	private static int contador = 1;
+	private static ArrayList <String> textos = new ArrayList<String>(1);
+	
 	/**
 	 * @param args
 	 */
@@ -20,7 +23,8 @@ final class ClaseMain {
 			x = "Estamos en Linux";
 			System.out.println(x);
 		}
-
+		
+		solicitarTexto();
 	}
 	
 
@@ -30,6 +34,32 @@ final class ClaseMain {
 
 	public static boolean esLinux() {
 		return (nombreSO.indexOf("nix") >= 0 || nombreSO.indexOf("nux") >= 0 || nombreSO.indexOf("aix") >= 0);
+	}
+	
+	public static void solicitarTexto() {
+		System.out.println("Ingrese el texto que quiera guardar:\nPara terminar de escribir, escriba \"exit\".\n");
+		texto();
+	}
+	
+	public static void texto() {
+		Scanner sc = new Scanner(System.in);
+		String texto1 = sc.nextLine();
+		textos.add(texto1);
+		contador++;
+		comprobarExits();
+	}
+	
+	public static void comprobarExits() {
+		for (String x : textos) {
+			if(x.indexOf("exit") <= 0 || x.indexOf("Exit") <= 0) {
+				exitOkey();
+			}
+		}
+		
+	}
+	
+	public static void exitOkey() {
+		System.out.println("Ingrese la ruta en la que quiere guardar el archivo: ");
 	}
 
 }

c73aaaae1ee2ab3104ea94ac7b15d96b4eac3cf8
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-19 20:36:44 +0200

    Actualizaci├│n autom├ítica

--- eclipse-workspace/EX08 Practica sumador/src/Lanzador.java
+++ eclipse-workspace/EX08 Practica sumador/src/Lanzador.java
@@ -3,68 +3,69 @@ import java.util.Random;
 
 public class Lanzador {
 
-	static final int NUM_PROCESOS = 1;
-	static final String PREFIJO_FICHEROS = "fich";
+    static final int NUM_PROCESOS = 1;
+    static final String PREFIJO_FICHEROS = "fich";
 
-	public static void lanzarSumador(int n1, int n2, String fichResultados) throws IOException {
-		String comando;
-		comando = "Sumador";
-		File directorioSumador;
-		directorioSumador = new File("C:\\Users\\Tomas\\Git\\JavaWorkSpaceConChema\\eclipse-workspace\\EX08 Practica sumador\\bin");
-		File fichResultado = new File(fichResultados);
-		ProcessBuilder pb;
-		
-		pb = new ProcessBuilder("java", comando, String.valueOf(n1), String.valueOf(n2));
-		pb.directory(directorioSumador);
-		pb.redirectOutput(fichResultado);
-		pb.start();
-	}
+    public static void lanzarSumador(int n1, int n2, String fichResultados) throws IOException {
+        String comando = "Sumador";
+        File directorioSumador = new File("C:\\Users\\Tomas\\Git\\JavaWorkSpaceConChema\\eclipse-workspace\\EX08 Practica sumador\\bin");
+        File fichResultado = new File(fichResultados);
+        ProcessBuilder pb;
+        String s1 = n1 + "";
+        String s2 = n2 + "";
+        pb = new ProcessBuilder("java", comando, s1, s2);
+        pb.directory(directorioSumador);
+        pb.redirectOutput(ProcessBuilder.Redirect.to(fichResultado));
+        pb.start();
+    }
 
-	public static int getResultadoFichero(String nombreFichero) {
-		int suma = 0;
-		try {
-			FileInputStream fichero = new FileInputStream(nombreFichero);
-			InputStreamReader fir = new InputStreamReader(fichero);
-			BufferedReader br = new BufferedReader(fir);
-			String linea = br.readLine();
-			suma = Integer.parseInt(linea);
-			return suma;
-		} catch (FileNotFoundException e) {
-			System.out.println("No se pudo abrir " + nombreFichero);
-		} catch (IOException e) {
-			System.out.println("No hay nada en " + nombreFichero);
-		}
-		return suma;
-	}
+    public static int getResultadoFichero(String nombreFichero) {
+        int suma = 0;
+        File file = new File(nombreFichero);
+        
+        if (file.exists() && file.length() > 0) {
+            try (FileInputStream fichero = new FileInputStream(file);
+                 InputStreamReader fir = new InputStreamReader(fichero);
+                 BufferedReader br = new BufferedReader(fir)) {
+                String linea = br.readLine();
+                suma = Integer.parseInt(linea);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("No se pudo abrir " + nombreFichero);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error al leer " + nombreFichero);
+            }
+        } else {
+            System.out.println("El archivo " + nombreFichero + " no existe o está vacío.");
+        }
+        
+        return suma;
+    }
 
-	public static int getSumaTotal(int numFicheros) {
-		int sumaTotal = 0;
-		for (int i = 1; i <= NUM_PROCESOS; i++) {
-			sumaTotal += getResultadoFichero(PREFIJO_FICHEROS + String.valueOf(i));
-		}
-		return sumaTotal;
-	}
 
-	/*
-	 * Recibe dos parámetros y hará la suma de los valores comprendidos entre ambos
-	 * parametros
-	 */
-	public static void main(String[] args) throws IOException, InterruptedException {
-		int n1 = Integer.parseInt(args[0]);
-		int n2 = Integer.parseInt(args[1]);
-		Random random = new Random();
-        int b = random.nextInt(10);
-        int b1 = random.nextInt(10);
-        n1 = n1 + b;
-        n2 = n2 + b1;
-		for (int i = 1; i <= NUM_PROCESOS; i++) {
-			System.out.println("n1:" + n1);
-			System.out.println("n2:" + n2);
-			lanzarSumador(n1, n2, PREFIJO_FICHEROS + String.valueOf(i));
-			System.out.println("Suma lanzada...");
-		}
-		Thread.sleep(1000);
-		int sumaTotal = getSumaTotal(NUM_PROCESOS);
-		System.out.println("La suma total es:" + sumaTotal);
-	}
+    public static int getSumaTotal(int numFicheros) {
+        int sumaTotal = 0;
+        for (int i = 1; i <= numFicheros; i++) {
+            sumaTotal += getResultadoFichero(PREFIJO_FICHEROS + String.valueOf(i));
+        }
+        return sumaTotal;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        Random random = new Random();
+        n1 = n1 + random.nextInt(10);
+        n2 = n2 + random.nextInt(10);
+        for (int i = 1; i <= NUM_PROCESOS; i++) {
+            System.out.println("n1: " + n1 + " el valor fue randomizado quedando así");
+            System.out.println("n2: " + n2 + " el valor fue randomizado quedando así");
+            lanzarSumador(n1, n2, PREFIJO_FICHEROS + String.valueOf(i));
+            System.out.println("Suma lanzada...");
+        }
+        Thread.sleep(1000);
+        int sumaTotal = getSumaTotal(NUM_PROCESOS);
+        System.out.println("La suma total es: " + sumaTotal);
+    }
 }
--- eclipse-workspace/EX08 Practica sumador/src/Sumador.java
+++ eclipse-workspace/EX08 Practica sumador/src/Sumador.java
@@ -1,15 +1,7 @@
 public class Sumador {
 
 	public static int sumar(int n1, int n2) {
-		int suma = 0;
-		if (n1 > n2) {
-			int aux = n1;
-			n1 = n2;
-			n2 = aux;
-		}
-		for (int i = n1; i <= n2; i++) {
-			suma = suma + i;
-		}
+		int suma = n1 + n2;
 		return suma;
 	}
 

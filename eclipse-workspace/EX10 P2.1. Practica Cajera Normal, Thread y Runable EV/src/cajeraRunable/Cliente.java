2b8d1568fae4c1d6d0ecea106cbdd6bc47e5b1d0
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-11-06 21:01:24 +0100

    Actualizaci├│n autom├ítica

---
+++ eclipse-workspace/EX10 P2.1. Practica Cajera Normal, Thread y Runable EV/src/cajeraRunable/Cliente.java
@@ -0,0 +1,28 @@
+package cajeraRunable;  //misma cliente normal
+public class Cliente {
+
+	private String nombre;
+	private int[] carroCompra;
+
+	Cliente(String nombre, int carroCompra[]){
+		this.nombre = nombre;
+		this.carroCompra = carroCompra;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int[] getCarroCompra() {
+		return carroCompra;
+	}
+
+	public void setCarroCompra(int[] carroCompra) {
+		this.carroCompra = carroCompra;
+	}
+
+}

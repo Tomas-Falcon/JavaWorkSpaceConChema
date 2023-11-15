53cd6766f81c9cf989208834581484992408707f
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-11-15 17:20:29 +0100

    Actualizaci├│n autom├ítica

--- eclipse-workspace/EX11 Base de Datos/src/SqlitePack/ProbandoSqlite.java
+++ eclipse-workspace/EX11 Base de Datos/src/SqlitePack/ProbandoSqlite.java
@@ -15,7 +15,10 @@ public class ProbandoSqlite {
 
    // ESTABLECER LA CONEXIÓN
    Connection conexion;
-   conexion = DriverManager.getConnection("jdbc:sqlite:bdpruebas.db");
+   //yo modifique esta ruta pera la coneccion, se le dice 
+   //que va a ser un jdbc de tipo sqlite y la direccion "jdbc:sqlite:C:\\Users\\Tomas\\Desktop\\Bases de datos\\bdpruebas.db"
+   //esta es la ruta original "jdbc:sqlite:bdpruebas.db" 
+   conexion = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Tomas\\Desktop\\Bases de datos\\bdpruebas.db");
 
    // CREAR ENUNCIADO
    Statement enunciado;
@@ -26,7 +29,8 @@ public class ProbandoSqlite {
    enunciado.execute("CREATE TABLE tablapruebas (id int primary key, aleatorio int);");
 
    // INSERTAR DATOS
-   for (int i = 1; i <= 100; i++) {
+   int i;
+   for (i = 1; i <= 100; i++) {
     enunciado.execute("INSERT INTO tablapruebas (id, aleatorio) values ("
         + i
         + ", "
@@ -36,20 +40,34 @@ public class ProbandoSqlite {
    // CONSULTA DATOS
    ResultSet resultados;
    resultados = enunciado.executeQuery("SELECT * FROM tablapruebas;");
+   
 
    // PROCESAR EL RESULTADO
    while (resultados.next()) {
-    System.out.println("id " + resultados.getString(1)
-      + ": aleatorio " + resultados.getString(2));
-   }
-
+	    System.out.println("id " + resultados.getString(1)
+	      + ": aleatorio " + resultados.getString(2));
+	   }
+   
+   resultados = enunciado.executeQuery("SELECT * FROM tablapruebas LIMIT 10;");
+   //selecciona los primeros 10 resultados
+   while (resultados.next()) {
+	   int id = Integer.parseInt(resultados.getString(1));
+	   int numero = Integer.parseInt(resultados.getString(2));
+	   
+	   enunciado.execute("INSERT INTO tablapruebas (id, aleatorio) values ("
+		        + id + ", " + numero);
+	    
+	   }
+   
+   //se unserta una copia de los primeros 10 resultados en las posiciones de 101 a 110
+   
    // CERRAR
    resultados.close();
    enunciado.close();
    conexion.close();
 
   } catch (Exception e) {
-   System.out.println("ERROR: " + e.getMessage());
+   System.out.println("ERROR: \"" + e.getMessage() + "\"  fin del error");
   }
 
  }

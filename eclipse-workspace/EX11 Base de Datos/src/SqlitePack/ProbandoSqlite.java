f483377b9459b91d4fb37dc646082f93f6fc2dd9
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-11-27 17:15:45 +0100

    Actualizaci├│n autom├ítica

--- eclipse-workspace/EX11 Base de Datos/src/SqlitePack/ProbandoSqlite.java
+++ eclipse-workspace/EX11 Base de Datos/src/SqlitePack/ProbandoSqlite.java
@@ -4,72 +4,71 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProbandoSqlite {
 
- public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException {
 
-  Class.forName("org.sqlite.JDBC");
+        Class.forName("org.sqlite.JDBC");
 
-  try {
+        try {
+            // ESTABLECER LA CONEXIÓN
+            Connection conexion = DriverManager.getConnection("jdbc:sqlite:bdpruebas.db");
 
-   // ESTABLECER LA CONEXIÓN
-   Connection conexion;
-   //yo modifique esta ruta pera la coneccion, se le dice 
-   //que va a ser un jdbc de tipo sqlite y la direccion "jdbc:sqlite:C:\\Users\\Tomas\\Desktop\\Bases de datos\\bdpruebas.db"
-   //esta es la ruta original "jdbc:sqlite:bdpruebas.db" 
-   conexion = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Tomas\\Desktop\\Bases de datos\\bdpruebas.db");
+            // CREAR ENUNCIADO
+            Statement enunciado = conexion.createStatement();
 
-   // CREAR ENUNCIADO
-   Statement enunciado;
-   enunciado = conexion.createStatement();
+            // CREAR UNA TABLA NUEVA, LA BORRA SI EXISTE
+            enunciado.execute("DROP TABLE IF EXISTS tablapruebas;");
+            enunciado.execute("CREATE TABLE tablapruebas (id INTEGER PRIMARY KEY, aleatorio INTEGER);");
 
-   // CREAR UNA TABLA NUEVA, LA BORRA SI EXISTE
-   enunciado.execute("DROP TABLE IF EXISTS tablapruebas;");
-   enunciado.execute("CREATE TABLE tablapruebas (id int primary key, aleatorio int);");
+            // INSERTAR DATOS
+            for (int i = 1; i <= 100; i++) {
+                enunciado.execute("INSERT INTO tablapruebas (id, aleatorio) values ("
+                        + i
+                        + ", "
+                        + Math.floor(Math.random() * 100 + 1) + ");");
+            }
 
-   // INSERTAR DATOS
-   int i;
-   for (i = 1; i <= 100; i++) {
-    enunciado.execute("INSERT INTO tablapruebas (id, aleatorio) values ("
-        + i
-        + ", "
-        + Math.floor(Math.random() * 100 + 1) + ");");
-   }
+            // CONSULTA DATOS
+            ResultSet resultados = enunciado.executeQuery("SELECT * FROM tablapruebas;");
 
-   // CONSULTA DATOS
-   ResultSet resultados;
-   resultados = enunciado.executeQuery("SELECT * FROM tablapruebas;");
-   
+            // Almacenar datos en una lista antes de cerrar el ResultSet
+            List<Integer> aleatorios = new ArrayList<>();
+            while (resultados.next()) {
+                aleatorios.add(resultados.getInt("aleatorio"));
+                System.out.println("id " + resultados.getString(1) + ": aleatorio " + resultados.getString(2));
+            }
 
-   // PROCESAR EL RESULTADO
-   while (resultados.next()) {
-	    System.out.println("id " + resultados.getString(1)
-	      + ": aleatorio " + resultados.getString(2));
-	   }
-   
-   resultados = enunciado.executeQuery("SELECT * FROM tablapruebas LIMIT 10;");
-   //selecciona los primeros 10 resultados
-   while (resultados.next()) {
-	   int id = Integer.parseInt(resultados.getString(1));
-	   int numero = Integer.parseInt(resultados.getString(2));
-	   
-	   enunciado.execute("INSERT INTO tablapruebas (id, aleatorio) values ("
-		        + id + ", " + numero);
-	    
-	   }
-   
-   //se unserta una copia de los primeros 10 resultados en las posiciones de 101 a 110
-   
-   // CERRAR
-   resultados.close();
-   enunciado.close();
-   conexion.close();
+            // INSERTAR DATOS 101-110 COPIANDO 1-10
+            String insertQuery = "INSERT INTO tablapruebas (id, aleatorio) VALUES (?, ?)";
+            PreparedStatement preparedStatement = conexion.prepareStatement(insertQuery);
 
-  } catch (Exception e) {
-   System.out.println("ERROR: \"" + e.getMessage() + "\"  fin del error");
-  }
+            for (int i = 101; i <= 110; i++) {
+                preparedStatement.setInt(1, i);
+                preparedStatement.setInt(2, aleatorios.get(i - 101)); // Utiliza los valores almacenados en la lista
+                preparedStatement.executeUpdate();
+            }
 
- }
+            // CONSULTA DATOS DESPUÉS DE LA INSERCIÓN
+            resultados = enunciado.executeQuery("SELECT * FROM tablapruebas;");
 
+            // PROCESAR EL RESULTADO
+            while (resultados.next()) {
+                System.out.println("id " + resultados.getString(1) + ": aleatorio " + resultados.getString(2));
+            }
+
+            // CERRAR
+            resultados.close();
+            enunciado.close();
+            preparedStatement.close();
+            conexion.close();
+
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
 }

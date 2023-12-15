90e06aa459b9cf9f25f07a215537c774f6c32789
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-12-15 21:04:31 +0100

    Actualizaci├│n autom├ítica

--- eclipse-workspace/EX09 Practicas Chema NE/src/sokets_Server_Connection/Cliente.java
+++ eclipse-workspace/EX09 Practicas Chema NE/src/sokets_Server_Connection/Cliente.java
@@ -1,25 +1,28 @@
 package sokets_Server_Connection;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Cliente {
-	public static void main(String[] args) {
-			try {
-				Socket s = new Socket("localhost", 0);
-				BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				System.out.println(br);
-				
-			}catch (UnknownHostException une) {
-				System.out.println("NO se encuentra el servidor");
-			}catch (IOException une) {
-				System.out.println("Error en Ia comunicación") ;
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 50165);
+            System.out.println("Conectado al servidor en el puerto: " + s.getLocalPort());
+
+            // Recibir datos del servidor
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String datosRecibidos = br.readLine();
+            System.out.println("Datos recibidos del servidor: " + datosRecibidos);
+
+            // Cerrar el socket del cliente
+            s.close();
+        } catch (UnknownHostException une) {
+            System.out.println("NO se encuentra el servidor");
+        } catch (IOException une) {
+            System.out.println("Error en la comunicación");
+        }
+    }
 }
-	
--- eclipse-workspace/EX09 Practicas Chema NE/src/sokets_Server_Connection/Servidor.java
+++ eclipse-workspace/EX09 Practicas Chema NE/src/sokets_Server_Connection/Servidor.java
@@ -1,20 +1,42 @@
 package sokets_Server_Connection;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
-	public static void main(String[] args) {
-	
 
-		try {
-			ServerSocket server = new ServerSocket(0) ;
-			Socket recepcion = server.accept() ;
-			}catch(){
-				
-			}
-		
-	}
-	
+    public static void main(String[] args) {
+        // Crear e iniciar un hilo para el servidor
+        Thread serverThread = new Thread(() -> iniciarServidor());
+        serverThread.start();
 
+        // Resto de la lógica del programa principal
+        System.out.println("Programa principal sigue ejecutándose...");
+    }
+
+    private static void iniciarServidor() {
+        try {
+            ServerSocket server = new ServerSocket(0);
+            System.out.println("Servidor esperando conexión en el puerto: " + server.getLocalPort());
+
+            while (true) {
+                // Esperar a que un cliente se conecte
+                Socket recepcion = server.accept();
+                System.out.println("Cliente conectado desde: " + recepcion.getRemoteSocketAddress());
+
+                // Enviar datos al cliente
+                String paquete = "Informacion que solicita la clase cliente";
+                PrintWriter out = new PrintWriter(recepcion.getOutputStream(), true);
+                out.println(paquete);
+
+                // Cerrar el socket del cliente
+                recepcion.close();
+            }
+        } catch (IOException e) {
+            // Manejo de la excepción de E/S (IOException)
+            e.printStackTrace();
+        }
+    }
 }
